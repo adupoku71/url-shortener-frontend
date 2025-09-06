9cc69f688e1be1e1009b63a524c54eb7282c74a8
adupoku71 <davidpoku71@gmail.com>
2025-09-06 12:06:25 +0000

    implement footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Footer() {
+  return (
+    <footer className="text-center my-20 text-gray-500 text-sm md:text-base">
+      <p>
+        &copy; 2025 ShortLink - Adu-Poku. Simple URL shortening for everyone.
+      </p>
+    </footer>
+  );
+}
