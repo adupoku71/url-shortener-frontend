e80e09d45adf9d8894a3bd28f3a607bca05407ca
adupoku71 <davidpoku71@gmail.com>
2025-09-06 11:43:34 +0000

    make homepage consistent with UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,17 @@ import ShortenForm from "@/components/ShortenForm";
 
 export default function HomePage() {
   return (
-    <main className="container mx-auto px-4 pt-0 pb-10">
-      <section className="min-height pt-10">
-        <div className="text-center mb-11">
-          <h2 className="md:text-5xl font-bold text-gray-900 mb-6 text-3xl sm:text-2xl">
-            Shorten. Share. Track.
-          </h2>
-          <p className="text-gray-600 md:text-xl max-w-2xl mx-auto text-base sm:text-lg">
-            Transform long URLs into short, shareable links and get detailed
-            analytics on every click
-          </p>
-        </div>
-        <ShortenForm />
-      </section>
+    <main className="container mx-auto px-4  py-10">
+      <div className="text-center mb-11">
+        <h2 className="md:text-5xl font-bold text-gray-900 mb-6 text-3xl sm:text-2xl">
+          Shorten. Share. Track.
+        </h2>
+        <p className="text-gray-600 md:text-xl max-w-2xl mx-auto text-base sm:text-lg">
+          Transform long URLs into short, shareable links and get detailed
+          analytics on every click
+        </p>
+      </div>
+      <ShortenForm />
       <QuickAccessLinks />
       <Features />
     </main>
